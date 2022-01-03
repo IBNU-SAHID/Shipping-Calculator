c00da3a0bb4e95c65fb3656b236d281fff421827
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2022-01-03 13:41:07 +0700

    create a shipping cost calculator based on the order amount.

---
+++ program/src/shippingCalculator.kt
@@ -0,0 +1,26 @@
+fun shippingCost(amount: Double, international: Boolean): Double {
+    var fee: Double
+    if ( international){
+        fee = amount * 0.15
+        if (fee > 50){
+            fee = 50.0
+        }
+    }else{
+        if (amount < 75 ) {
+            fee = amount * 0.1
+        }else {
+            fee =  0.0
+        }
+    }
+    return fee
+}
+fun main(args: Array<String>) {
+    println("============== Shipping Calculator ==============")
+    print("Order Amount : ")
+    val total = readLine()!!.toDouble()
+    print("International (true/false) : ")
+    val  international = readLine()!!.toBoolean()
+    println("------------------------------------------------")
+    print("Shipping Cost : ")
+    println(shippingCost(total, international))
+}
